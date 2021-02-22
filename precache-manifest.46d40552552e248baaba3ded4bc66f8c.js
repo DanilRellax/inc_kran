56a18fddc98710b3859527bc86eefe2643795735
DanilRellax <77833594+DanilRellax@users.noreply.github.com>
2021-02-22 14:56:19 +0200

    Updates

---
+++ precache-manifest.46d40552552e248baaba3ded4bc66f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2df90620572af03ff544f9c2cd67bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b9d2c1b63c83741546dd",
+    "url": "./static/css/2.fb75dcb6.chunk.css"
+  },
+  {
+    "revision": "b9d2c1b63c83741546dd",
+    "url": "./static/js/2.680ce764.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "./static/js/2.680ce764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7036d6005f616ef0c4f3",
+    "url": "./static/js/main.b47ccb24.chunk.js"
+  },
+  {
+    "revision": "5416c08adc152c0560ee",
+    "url": "./static/js/runtime-main.b3720eee.js"
+  }
+]);
